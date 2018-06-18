accdaa2d279c1f99cb7552fb7926a66d83b10ddf
SeonheeKim <dnwdjqldb@cnu.ac.kr>
2018-06-18 13:43:59 +0900

    헤더까지 development 빌드 성공

---
+++ server/main.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import path from 'path';
+
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+import api from './routes';
+
+const app = express();
+const port = 3000;
+
+const devPort = 4000;
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+mongoose.connect('mongodb://localhost/codelab');
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'CodeLab1$1$234',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/api', api);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/hello', (req, res) => {
+    return res.send('Hello CodeLab');
+});
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
